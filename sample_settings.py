3e803358cae305f6aafcc020e10f0877fbd63db8
Eric <eric@ericstein.org>
2013-04-29 06:07:22 -0400

    added models file, additional settings.

--- sample_settings.py
+++ sample_settings.py
@@ -1,10 +1,36 @@
+import os
 # Define data used in both Development and production
 ADMIN = 'Eric'
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class Development(object):
     DEBUG = True
     ADMIN = ADMIN
+    BASEDIR = BASEDIR
+
+    # local database login information
+    configuration = {
+        'db_user': '',
+        'db_password': '',
+        'port': 5432,
+        'host': 'localhost',
+        'db_name': 'website'
+    }
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{db_user}:{db_password}@{host}:{port}/{db_name}'.format(**configuration)
+
 
 class Production(object):
     DEBUG = False
     ADMIN = ADMIN
+    BASEDIR = BASEDIR
+
+    # local database login information
+    configuration = {
+        'db_user': '',
+        'db_password': '',
+        'port': 5432,
+        'host': 'localhost',
+        'db_name': 'website'
+    }
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{db_user}:{db_password}@{host}:{port}/{db_name}'.format(**configuration)
--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask('website')
+db = SQLAlchemy(app)
 
 
 @app.route('/')
